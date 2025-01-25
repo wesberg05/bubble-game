374fc210ff9bb29cda24d044582f8387ac48df01
Wesley Berg <wesberg05@gmail.com>
2025-01-25 11:03:06 -0400

    removed health system

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -3,67 +3,16 @@ using UnityEngine.UI;
 
 public class HealthSystem : MonoBehaviour
 {
+    [Header("Health Settings")]
     [SerializeField] private int maxHealth = 3;
-    [SerializeField] private Image[] heartImages;
     [SerializeField] private Sprite fullHeart;
     [SerializeField] private Sprite emptyHeart;
 
-    private int currentHealth;
-
-    private void Start()
-    {
-        currentHealth = maxHealth;
-        UpdateHeartUI();
-    }
-
-    public void TakeDamage(int damage)
-    {
-        currentHealth -= damage;
-        currentHealth = Mathf.Max(currentHealth, 0); // Prevent negative health
-        UpdateHeartUI();
-
-        if (currentHealth <= 0)
-        {
-            Die();
-        }
-    }
-
-    private void UpdateHeartUI()
-    {
-        for (int i = 0; i < heartImages.Length; i++)
-        {
-            if (i < currentHealth)
-            {
-                heartImages[i].sprite = fullHeart;
-            }
-            else
-            {
-                heartImages[i].sprite = emptyHeart;
-            }
-        }
-    }
-
-    private void Die()
-    {
-        // Disable the witch game object
-        gameObject.SetActive(false);
-        
-        // You might want to add game over logic here
-        Debug.Log("Witch has died!");
-    }
-}
-using UnityEngine;
-using UnityEngine.UI;
-
-public class HealthSystem : MonoBehaviour
-{
-    [SerializeField] private GameObject heartPrefab;
-    [SerializeField] private Sprite fullHeart;
-    [SerializeField] private Sprite emptyHeart;
+    [Header("UI References")]
+    [SerializeField] private GameObject heartPrefab; // Prefab for heart
     [SerializeField] private float heartSpacing = 0.5f;
-    [SerializeField] private Vector2 heartsOffset = new Vector2(0, -0.5f); // Offset below the witch
+    [SerializeField] private Vector2 heartsOffset = new Vector2(0, -0.5f);
 
-    private int maxHealth = 3;
     private int currentHealth;
     private Image[] heartImages;
     private Canvas worldSpaceCanvas;
@@ -82,11 +31,11 @@ public class HealthSystem : MonoBehaviour
         GameObject canvasObj = new GameObject("HealthCanvas");
         canvasObj.transform.SetParent(transform);
         canvasObj.transform.localPosition = Vector3.zero;
-        
+
         worldSpaceCanvas = canvasObj.AddComponent<Canvas>();
         worldSpaceCanvas.renderMode = RenderMode.WorldSpace;
         worldSpaceCanvas.sortingOrder = 1;
-        
+
         // Add a canvas scaler
         CanvasScaler scaler = canvasObj.AddComponent<CanvasScaler>();
         scaler.dynamicPixelsPerUnit = 100f;
@@ -102,7 +51,7 @@ public class HealthSystem : MonoBehaviour
         heartImages = new Image[maxHealth];
         for (int i = 0; i < maxHealth; i++)
         {
-            GameObject heartObj = Instantiate(heartPrefab, panelObj.transform);
+            GameObject heartObj = Instantiate(heartPrefab, heartsPanel);
             RectTransform heartTransform = heartObj.GetComponent<RectTransform>();
             heartTransform.anchoredPosition = new Vector2(i * heartSpacing * 100, 0);
             heartImages[i] = heartObj.GetComponent<Image>();
@@ -112,7 +61,7 @@ public class HealthSystem : MonoBehaviour
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
-        currentHealth = Mathf.Max(currentHealth, 0);
+        currentHealth = Mathf.Max(currentHealth, 0); // Prevent negative health
         UpdateHeartUI();
 
         if (currentHealth <= 0)
